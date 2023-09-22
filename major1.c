c490093d0581281c799ab2d8f9022833417a4066
bibekbajagain8 <101430528+bibekbajagain8@users.noreply.github.com>
2023-09-21 20:49:23 -0500

    basic major1 menu

--- major1.c
+++ major1.c
@@ -1,6 +1,24 @@
 #include "major1.h"
 
 int main() {
-    
+    int choice;
+
+    do {
+
+        /* Preview menu  */
+        printf("Enter the menu option for the operation to perform:\n");
+        printf("(1) Count Leading Zeroes\n");
+        printf("(2) Endian Swap\n");
+        printf("(3) Rotate-right\n");
+        printf("(4) Parity\n");
+        printf("(5) EXIT\n");
+
+        /* Take choice from keyboard  */
+        scanf("%d", &choice);
+
+        switch (choice) {
+            
+        }
+    }while (choice != 5);    
     return 0;
 }
